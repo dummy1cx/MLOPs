93f90ac42b4037a8669bf1e4cf0fdd8177e44fad
Abhishek <dummy1cx@gmail.com>
2025-06-04 10:47:31 +0100

    dataset creation

---
+++ mycode.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+## Create a simple dataframe with columns names
+
+data = {
+    'Name' : ['Alice', 'Bob', 'Charlie'],
+    'Age' : [25,30,35],
+    'City' : ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+
+
+## Ensure the data directory exists at the root level
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok = True)
+
+
+## Defien the ffile path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+## Save the Dataframe to a CSV files, including columns name
+df.to_csv(file_path, index = False)
+print(f"CSV file saved to {file_path}")
